94e12d79bb569dfc432dd05dbe723bbce5a5b1a6
sihun oh <ohsh0731@gmail.com>
2025-08-08 22:04:54 +0900

    [feat] project setting

---
+++ dgb-back/src/main/java/com/ddalggack/dgbback/DgbBackApplication.java
@@ -0,0 +1,13 @@
+package com.ddalggack.dgbback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgbBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DgbBackApplication.class, args);
+    }
+
+}
---
+++ dgb-back/src/test/java/com/ddalggack/dgbback/DgbBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddalggack.dgbback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgbBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
